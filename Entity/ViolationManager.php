a440a7011af77a09870cbc6afdc2097ce34a6449
Jérémy Romey <jeremy@free-agent.fr>
2013-04-02 20:10:47 +0200

    Added notifiedAt

--- Entity/ViolationManager.php
+++ Entity/ViolationManager.php
@@ -70,6 +70,86 @@ class ViolationManager implements ViolationManagerInterface
     }
 
     /**
+     * {@inheritdoc}
+     */
+    public function getViolations(array $params = array())
+    {
+        $order = isset($params['order']) && in_array(strtoupper($params['order']), array('ASC', 'DESC')) ? $params['order'] : 'DESC';
+
+        $queryBuilder = $this
+            ->getObjectManager()
+            ->getRepository($this->violationClass)
+            ->createQueryBuilder('v')
+            ->orderBy('v.createdAt', $order)
+        ;
+
+        if (isset($params['subject'])) {
+            $subject = $params['subject'];
+            $params['subject_model'] = $this->getClassForModel($subject);
+            $params['subject_id']    = $subject->getId();
+        }
+
+        if (isset($params['subject_model'])) {
+            $queryBuilder
+                ->andWhere('v.subjectModel = :subjectModel')
+                ->setParameter('subjectModel', $params['subject_model'])
+            ;
+        }
+
+        if (isset($params['subject_id'])) {
+            if (is_array($params['subject_id'])) {
+                if (isset($params['subject_model'])) {
+                    if (!empty($params['subject_id'])) {
+                        $queryBuilder
+                            ->andWhere('v.subjectId IN (:subjectId)')
+                            ->setParameter('subjectId', $params['subject_id'])
+                        ;
+                    } else {
+                        $queryBuilder->andWhere('v.code IS NULL');
+                    }
+                }
+            } else {
+                $queryBuilder
+                    ->andWhere('v.subjectId = :subjectId')
+                    ->setParameter('subjectId', $params['subject_id'])
+                ;
+            }
+        }
+
+        if (isset($params['codes'])) {
+            $codes = $params['codes'];
+            if (!is_array($codes)) {
+                $codes = array($codes);
+            }
+            if (!empty($codes)) {
+                $queryBuilder
+                    ->andWhere('v.code IN (:codes)')
+                    ->setParameter('codes', $codes)
+                ;
+            } else {
+                $queryBuilder->andWhere('v.code IS NULL');
+            }
+        }
+
+        if (isset(($params['is_fixed'] && is_bool($params['is_fixed']))) {
+            $queryBuilder
+                ->andWhere('v.fixed = :fixed')
+                ->setParameter('fixed', $params['is_fixed'])
+            ;
+        }
+
+        if (isset(($params['is_notified'])) {
+            if (true == $params['is_notified']) {
+                $queryBuilder->andWhere('v.notifiedAt IS NOT NULL');
+            } else {
+                $queryBuilder->andWhere('v.notifiedAt IS NULL');
+            }
+        }
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
      * @return ObjectManager
      */
     public function getObjectManager()
